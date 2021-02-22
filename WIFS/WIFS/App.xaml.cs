ced2480538b78f4a7b4a415a45b82097a4bcb04a
moskito78 <kenshukimu@gmail.com>
2021-02-22 17:01:34 +0900

    프로세스 확인처리

--- WIFS/WIFS/App.xaml.cs
+++ WIFS/WIFS/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,48 @@ namespace WIFS
     /// </summary>
     public partial class App : Application
     {
+        public bool IsProcessOpen(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.Contains(name))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Get Reference to the current Process
+            Process thisProc = Process.GetCurrentProcess();
+
+            if (IsProcessOpen("WIFS") == false)
+            {
+                //System.Windows.MessageBox.Show("Application not open!");
+                //System.Windows.Application.Current.Shutdown();
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else
+            {
+                // Check how many total processes have the same name as the current one
+                if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
+                {
+                    // If ther is more than one, than it is already running.
+                    System.Windows.MessageBox.Show("프로그램이 실행 중에 있습니다.");
+                    System.Windows.Application.Current.Shutdown();
+                    return;
+                }
+                else
+                {
+                    var mainWindow = new MainWindow();
+                    mainWindow.Show();
+                }
+
+                base.OnStartup(e);
+            }
+        }
     }
 }
